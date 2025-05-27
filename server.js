76f6e52dee5c795f2b9e7e23ab8057932abf5d7e
Mani0415 <maniselvan231@gmail.com>
2025-05-27 18:23:33 +0530

    server update

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoutes.js";
 import userRoute from "./routes/userRoutes.js";
-import { dirname } from "path";
+import { dirname, resolve, join } from "path";
 import { fileURLToPath } from "url";
-import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 dotenv.config();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
+app.use("/uploads", express.static(resolve(__dirname, "uploads")));
 
+const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
 app.use(
   cors({
     origin: allowedOrigin,
@@ -31,19 +35,31 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const _dirname = dirname(fileURLToPath(import.meta.url));
-app.use("/uploads", express.static(path.resolve(_dirname, "./uploads")));
-
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 
-const port = process.env.PORT || 3010;
+const publicPath = resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
+console.log("public path: ", publicPath);
+
+app.get("/", (req, res) => {
+  const filePath = join(__dirname, "public", "index.html");
+  console.log(filePath);
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).send("index.html not found");
+  }
+});
+
+const port = process.env.PORT || 3010;
 app.listen(port, () => {
   try {
-    console.log(`server is running port on ${port}`);
-    console.log("database connected");
+    console.log(`✅ Server is running on port ${port}`);
+    console.log("✅ Database connected");
   } catch (error) {
-    console.log(error);
+    console.error("❌ Server start error:", error);
   }
 });
